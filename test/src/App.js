ccac0ba2c0db15bb77e5d2108d1a3795fc2770bc
Witcher-MTM <phantomit228@gmail.com>
2023-04-07 13:54:01 +0300

    Update App.js

--- test/src/App.js
+++ test/src/App.js
@@ -6,21 +6,22 @@ function Send(order)
   const requestOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json'},
-    
     body: JSON.stringify({ 
         order
         })  
   };
-  fetch('http://localhost:3000/order/',requestOptions);
   console.log('send');
+  fetch('http://localhost:3000/order/',requestOptions);
 } 
+
 function App() {
   const [name,setName] = useState('');
   const [phone,setPhone] = useState('');
   const [email,setEmail] = useState('');
   const [datetime,setDatetime] = useState('');
   const [confirmType,setConfirmType] = useState('');
-  const order = {name:name,phone:phone,email:email,datetime:datetime,confirmType:confirmType}
+
+  const order = {name:name,phone:phone,email:email,datetime:datetime,confirmType:confirmType,isConfirm:false}
 
   return (
     <div className="App">
@@ -29,8 +30,13 @@ function App() {
           <input placeholder="Phone"value = {phone} onChange={(e)=>setPhone(e.target.value)} type="phone"></input>
           <input placeholder="email" value = {email} onChange={(e)=>setEmail(e.target.value)} type="email"></input>
           <input placeholder="datetime" value = {datetime} onChange={(e)=>setDatetime(e.target.value)} type="date"></input>
-          <input placeholder="confrimtype" value = 'Telegram' onChange={(e)=>setConfirmType(e.target.value)} type="checkbox"></input>
-          <button onClick={() => {Send(order)}}>Send order</button>
+          <input type="checkbox" id="telegramCheckbox" name="telegram" value="Telegram" onChange={(e)=>setConfirmType(e.target.value)}></input>
+          <label for="telegramCheckbox">Telegram</label>
+          <br></br>
+          <input type="checkbox" id="gmailCheckbox" name="gmail" value="Gmail" onChange={(e)=>setConfirmType(e.target.value)}></input>
+          <label for="gmailCheckbox">Gmail</label>
+          <br></br>
+           <button onClick={() => {Send(order)}}>Send order</button>
       </div>
     </div>
   );  
